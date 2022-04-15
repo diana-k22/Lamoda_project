9104c2975792cbe9759e7deebc767a898d561930
diana-k22 <75387147+diana-k22@users.noreply.github.com>
2022-04-16 01:30:37 +0300

    changed class capital letter to small

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,35 +12,35 @@ library.add( fab, fas, faHome, faMapMarker)
 const Header = () => {
     return (
         
-        <div className='Header_wrapper'>
-            <div className='Header container'>
-                <div className='Header_delivery__hover'>
-                    <span className='Header_delivery__text'>Укажите регион доставки</span>
+        <div className='header_wrapper'>
+            <div className='header container'>
+                <div className='header_delivery'>
+                    <span className='header_delivery__text'>Укажите регион доставки</span>
                     <FontAwesomeIcon icon="fa-solid fa-angle-down" className='Icon'/>
                 </div>
-                <div className="Header_link">
-                    <a href="#" className='Header_link__hover'>
+                <div className="header_link">
+                    <a href="#" className='header_link__hover'>
                         <FontAwesomeIcon icon="fa-solid fa-mask-face" className='Icon'/>
-                        <span className='Header_link__text'>Бесконтактная доставка с примеркой</span>
-                        <div className='Header_modal__none'>
-                            <p className='Modal_text'>Lorem ipsum dolor sit amet consectetur.</p>
-                            <p className='Modal_text_h'>Подробнее</p>
+                        <span className='header_link__text'>Бесконтактная доставка с примеркой</span>
+                        <div className='header_modal__none'>
+                            <p className='modal_text'>Lorem ipsum dolor sit amet consectetur.</p>
+                            <p className='modal_text_h'>Подробнее</p>
                         </div>
                     </a>
-                    <a href="#" className='Header_link__hover'>
+                    <a href="#" className='header_link__hover'>
                         <FontAwesomeIcon icon="fa-solid fa-credit-card" className='Icon' />
-                        <span className='Header_link__text'>Платите когда хотите</span>
-                        <div className='Header_modal__none'>
-                            <p className='Modal_text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam cum in assumenda velit blanditiis.</p>
-                            <p className='Modal_text_h'>Подробнее</p>
+                        <span className='header_link__text'>Платите когда хотите</span>
+                        <div className='header_modal__none'>
+                            <p className='modal_text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam cum in assumenda velit blanditiis.</p>
+                            <p className='modal_text_h'>Подробнее</p>
                         </div>
                     </a>
-                    <a href="#" className='Header_link__hover'>
+                    <a href="#" className='header_link__hover'>
                     <FontAwesomeIcon icon="fa-solid fa-clock"className='Icon' />
-                        <span className='Header_link__text'>Как мы работаем сейчас</span>
-                        <div className='Header_modal__none'>
-                            <p className='Modal_text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus, ducimus?</p>
-                            <p className='Modal_text_h'>Подробнее</p>
+                        <span className='header_link__text'>Как мы работаем сейчас</span>
+                        <div className='header_modal__none'>
+                            <p className='modal_text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus, ducimus?</p>
+                            <p className='modal_text_h'>Подробнее</p>
                         </div>
                     </a>
                 </div>
